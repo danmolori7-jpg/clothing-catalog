60a9df6bbef3d69c6d68f247dace0dabf5dea090
Danil Nadtochey <dan.nadtochey@mail.ru>
2026-09-09 13:01:17 +0300

    chore: remove starter content

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+const rootElement = document.getElementById("root");
+//getElementById HTMLElement | null поэтому выполняем сужение типа дальше ТС понимает что у нас  HTMLElement
+
+if (rootElement === null) {
+  throw new Error("Не найден элемент root");
+}
+
+createRoot(rootElement).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
